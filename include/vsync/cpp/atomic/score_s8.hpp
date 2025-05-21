ba54c8c43cc402aa10e30fb834816645e9d96393
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-05-21 18:56:55 +0300

    WIP: support signed
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- include/vsync/cpp/atomic/score_s8.hpp
+++ include/vsync/cpp/atomic/score_s8.hpp
@@ -28,6 +28,121 @@ namespace vsync
             _a.store(static_cast<vuint8_t>(v), order);
         }
 
+        vint8_t operator=(vint8_t v) noexcept
+        {
+            store(v);
+            return v;
+        }
+
+        operator vint8_t() const noexcept
+        {
+            return load();
+        }
+
+        vint8_t exchange(vint8_t v,
+                         memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.exchange(static_cast<vuint8_t>(v), order));
+        }
+
+
+        bool compare_exchange_strong(
+            vint8_t &expected, vint8_t desired,
+            memory_order order   = memory_order_seq_cst,
+            memory_order failure = memory_order_seq_cst) noexcept
+        {
+            // TODO: find a way to make the cast on expected safe!
+            return _a.compare_exchange_strong((vuint8_t &)(expected),
+                                              static_cast<vuint8_t>(desired),
+                                              order, failure);
+        }
+        bool compare_exchange_weak(
+            vint8_t &expected, vint8_t desired,
+            memory_order order   = memory_order_seq_cst,
+            memory_order failure = memory_order_seq_cst) noexcept
+        {
+            return compare_exchange_strong(expected, desired, order, failure);
+        }
+
+        vint8_t fetch_add(vint8_t v,
+                          memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.fetch_add(static_cast<vuint8_t>(v), order));
+        }
+        vint8_t operator+=(vint8_t v) noexcept
+        {
+            return fetch_add(v);
+        }
+        // v++
+        vint8_t operator++(int) noexcept
+        {
+            return static_cast<vint8_t>(_a++);
+        }
+        // ++v
+        vint8_t operator++() noexcept
+        {
+            return static_cast<vint8_t>(++_a);
+        }
+
+        vint8_t fetch_sub(vint8_t v,
+                          memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.fetch_sub(static_cast<vuint8_t>(v), order));
+        }
+        vint8_t operator-=(vint8_t v) noexcept
+        {
+            return fetch_sub(v);
+        }
+        // v--
+        vint8_t operator--(int) noexcept
+        {
+            return static_cast<vint8_t>(_a--);
+        }
+        // --v
+        vint8_t operator--() noexcept
+        {
+            return static_cast<vint8_t>(--_a);
+        }
+
+        vint8_t fetch_and(vint8_t v,
+                          memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.fetch_and(static_cast<vuint8_t>(v), order));
+        }
+
+        vint8_t operator&=(vint8_t v) noexcept
+        {
+            return fetch_and(v);
+        }
+
+        vint8_t fetch_or(vint8_t v,
+                         memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.fetch_or(static_cast<vuint8_t>(v), order));
+        }
+
+        vint8_t operator|=(vint8_t v) noexcept
+        {
+            return fetch_or(v);
+        }
+
+        vint8_t fetch_xor(vint8_t v,
+                          memory_order order = memory_order_seq_cst) noexcept
+        {
+            return static_cast<vint8_t>(
+                _a.fetch_xor(static_cast<vuint8_t>(v), order));
+        }
+
+        vint8_t operator^=(vint8_t v) noexcept
+        {
+            return fetch_xor(v);
+        }
+
       private:
         vsync::atomic<vuint8_t> _a;
     };
