ae36da1cf9443fb1a9de9e884d35bb8d1a228d13
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-06-03 15:50:50 +0300

    test
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- test/atomics_cxx/ut_volatile.cpp
+++ test/atomics_cxx/ut_volatile.cpp
@@ -11,8 +11,12 @@ main(void)
     volatile std::atomic<vuint32_t> a;
     volatile vsync::atomic<vuint32_t> b;
 
+    volatile std::atomic<int> x;
+    volatile vsync::atomic<int> y;
     a++;
     b++;
+    x++;
+    y++;
 
     return 0;
 }
