97a224bfc089a72c115cc93a21e1592410dfae60
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-06-03 15:48:38 +0300

    WIP: support volatile
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- test/atomics_cxx/ut_volatile.cpp
+++ test/atomics_cxx/ut_volatile.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Huawei Technologies Co., Ltd. . All rights reserved.
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
  * SPDX-License-Identifier: MIT
  */
 #include <atomic>
