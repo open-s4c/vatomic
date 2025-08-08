ca2df1e922274f464ff47c982ce0271f5cc981b7
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-08-08 17:21:05 +0300

    Commit example
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

---
+++ include/vsync/doc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * Description: VSync API documention
+ * Author: Huawei Dresden Research Center
+ */
+#ifndef VSYNC_DOC_H
+#define VSYNC_DOC_H
+/*******************************************************************************
+ * @dir vsync
+ *
+ * vatomic is a header library of atomics operations, supporting mainstream
+ * architectures: ARMv7, ARMv8 (AArch32 and AArch64), RISC-V, and x86_64. The
+ * memory ordering guarantees provided by the atomic interface are formally
+ * described in the VSync Memory Model (VMM) file.
+ *
+ ******************************************************************************/
+#endif
