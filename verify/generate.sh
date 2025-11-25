f40841faeae59bc404b222e8ea8a8817d3423339
Diogo Behrens <diogo.behrens@huawei.com>
2025-11-25 11:51:48 +0000

    scripts: Tidy up verification scripts
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- verify/generate.sh
+++ verify/generate.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
-if [[ $# -ne 2 ]]; then
+if [ $# -ne 2 ]; then
   echo "Usage: $0 FUNCTION_NAME ARCH"
   exit 1
 fi
@@ -9,42 +9,38 @@ fi
 FUNC="$1"
 ARCH="$2"
 
-declare -A ARCHS
-ARCHS=(
-  ["armv8"]="./armv8/atomics.s"
-  ["riscv"]="./riscv/atomics.s"
-)
+case "${ARCH}" in
+	armv8)
+		ARCHS="./armv8/atomics.s"
+		;;
+	riscv)
+		ARCHS="./riscv/atomics.s"
+		;;
+	*)
+		echo "Unknown architecture: $ARCH"
+		exit 1
+		;;
+esac
 
-if [[ -z "${ARCHS[$ARCH]+x}" ]]; then
-  echo "Unknown architecture: $ARCH"
-  exit 1
-fi
-
+UNROLL=
 OUT="out/$ARCH"
 OUT_RETRY="out_retry/$ARCH"
 TMPFUNCS=$(mktemp)
 echo "$FUNC" > "$TMPFUNCS"
 
 compile() {
-  local outdir="$1"
-  local unroll="$2"
-  echo "Generating $FUNC for $ARCH (unroll=$unroll, outdir=$outdir)"
-  if [[ "$unroll" == "true" ]]; then
-    cargo run --quiet -- \
-      --input "${ARCHS[$ARCH]}" \
-      --functions "$TMPFUNCS" \
-      --templates ./boogie/templates/ \
-      --directory "$outdir" \
-      --arch "$ARCH" \
-      --unroll
-  else
-    cargo run --quiet -- \
-      --input "${ARCHS[$ARCH]}" \
-      --functions "$TMPFUNCS" \
-      --templates ./boogie/templates/ \
-      --directory "$outdir" \
-      --arch "$ARCH"
-  fi
+	outdir="$1"
+	unroll="$2"
+	if $unroll; then
+		UNROLL="--unroll"
+	fi
+	echo "Generating $FUNC for $ARCH (unroll=$unroll, outdir=$outdir)"
+	cargo run --quiet -- \
+		--input "${ARCHS}" \
+		--functions "$TMPFUNCS" \
+		--templates ./boogie/templates/ \
+		--directory "$outdir" \
+		--arch "$ARCH" $UNROLL
 }
 
 # generate for both phases
