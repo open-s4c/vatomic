09e8820d4b1cdef0cf00303bce24f04d803ed2ff
Diogo Behrens <diogo.behrens@huawei.com>
2025-11-27 10:02:53 +0000

    doc: Add doxygen and markdown targets independently
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -9,27 +9,20 @@ function(add_doc_targets)
     find_program(MDOX_INSTALLED mdox DOC "mdox")
     find_program(DOXYGEN_INSTALLED doxygen DOC "doxygen")
     set(MARKDOWN_TARGET "markdown")
+    set(DOXYGEN_TARGET "doxygen")
 
+    # ##########################################################################
+    # Add doxygen target Run with: make doxygen
+    # ##########################################################################
     if(NOT DOXYGEN_INSTALLED)
         message(
             WARNING
-                "Target `${MARKDOWN_TARGET}` was not added! doxygen is not installed. Please install https://doxygen.nl/download.html"
-        )
-        return()
-    endif()
-
-    if(NOT MDOX_INSTALLED)
-        message(
-            WARNING
-                "Target `${MARKDOWN_TARGET}` was not added! mdox is not installed. Please install https://github.com/db7/mdox/releases/tag/v0.1"
+                "Target `${DOXYGEN_TARGET}` disabled! Please install https://doxygen.nl/download.html"
         )
         return()
     endif()
 
     set(DOXYGEN_DOCKER "")
-    # ##########################################################################
-    # Generate doxygen config file
-    # ##########################################################################
     set(DOXYGEN_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/doxygen")
     set(DOXYGEN_FILE_IN "${PROJECT_SOURCE_DIR}/doc/Doxyfile.in")
     set(DOXYGEN_FILE "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
@@ -37,10 +30,6 @@ function(add_doc_targets)
     # Note that this will substitute @VAR@ with the value of VAR in
     # ${DOXYGEN_FILE_IN} and write the outout to ${DOXYGEN_FILE}
     configure_file(${DOXYGEN_FILE_IN} ${DOXYGEN_FILE})
-    # ##########################################################################
-    # Add doxygen target Run with: make doxygen
-    # ##########################################################################
-    set(DOXYGEN_TARGET "doxygen")
     # create output directory
     file(MAKE_DIRECTORY "${DOXYGEN_OUTPUT}")
     add_custom_target(
@@ -49,9 +38,19 @@ function(add_doc_targets)
         WORKING_DIRECTORY "${DOXYGEN_INPUT}"
         COMMENT "Generating Doxygen documentation for include folder."
         VERBATIM)
+
     # ##########################################################################
     # Add markdown target Run with: make markdown
     # ##########################################################################
+
+    if(NOT MDOX_INSTALLED)
+        message(
+            WARNING
+                "Target `${MARKDOWN_TARGET}` disabled! Please install https://github.com/db7/mdox"
+        )
+        return()
+    endif()
+
     set(DOC_OUTPUT "${PROJECT_SOURCE_DIR}/doc/api")
     set(MDOX mdox)
     add_custom_target(
